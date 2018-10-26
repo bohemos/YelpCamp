8b5f99cecca467b60bf47f9cf5ac0286a3b22122
Jeffrey Shin <jeffspectre@gmail.com>
2018-10-26 02:57:33 +0000

    refactor routes

---
+++ routes/campgrounds.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var router  = express.Router();
+var Campground = require("../models/campground");
+
+//INDEX - show all campgrounds
+router.get("/", function(req, res){
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            //res.render("campgrounds/index.ejs", {campgrounds: allCampgrounds, currentUser: req.user});   //can get rid of this becuase of the app.use() middleware
+            res.render("campgrounds/index.ejs", {campgrounds: allCampgrounds});
+        }
+    });
+});
+
+//CREATE - add new campground to DB
+router.post("/", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - show form to create new campground
+router.get("/new", function(req, res){
+    res.render("campgrounds/new.ejs");
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("campgrounds/show.ejs", {campground: foundCampground});
+        }
+    });
+});
+
+module.exports = router;
+//took out "/campgrounds" as there is a link to in with router in app.js
